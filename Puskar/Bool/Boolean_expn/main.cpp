7504579bcc6dbd7414df9ff70f770e19b813f192
Puskar-Neupane <84566897+Puskar-Neupane@users.noreply.github.com>
2021-07-27 17:27:40 +0545

    Create main.cpp

---
+++ Puskar/Bool/Boolean_expn/main.cpp
@@ -0,0 +1,27 @@
+#include "mainwindow.h"
+#include "Expression.h"
+#include<iostream>
+#include<cmath>
+#include<string.h>
+using namespace std;
+
+#include <QApplication>
+//int expn();
+int main(int argc, char *argv[])
+{
+
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+   // expn();
+    return a.exec();
+
+}
+
+
+
+
+
+
+
+
