b2ebbdeed5985d5357d4194b2a9e9bbe00ef35f2
Puskar-Neupane <84566897+Puskar-Neupane@users.noreply.github.com>
2021-07-29 23:25:49 +0545

    Update Expression.h

--- Puskar/Bool/Boolean_expn/Expression.h
+++ Puskar/Bool/Boolean_expn/Expression.h
@@ -4,22 +4,21 @@
 
 #include<iostream>
 using namespace std;
-extern  int result[4], X[4][4];
+extern  int result[16], X[16][4],n;
 namespace puskar
 {
 
     static string S,P;
 }
-class Expression
+class expression
 {
     public:
     string expression, var, single_variable;
-    int length;
-    int n = 0, n2 = 0; //n is number_of_variables
+    int length; //length of expression
+     //number of variables
     void get();
     void analyze();
 };
-
 
 
 #endif // EXPRESSION_H
