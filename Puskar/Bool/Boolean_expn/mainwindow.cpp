89ef4c3fc2a9e56f792eb5462c4f14197795b19c
Puskar-Neupane <84566897+Puskar-Neupane@users.noreply.github.com>
2021-07-29 23:25:59 +0545

    Update mainwindow.cpp

--- Puskar/Bool/Boolean_expn/mainwindow.cpp
+++ Puskar/Bool/Boolean_expn/mainwindow.cpp
@@ -26,7 +26,7 @@ TestModel::TestModel(QObject *parent) : QAbstractTableModel(parent)
 }
 
 // Create a method to populate the model with data:
-void TestModel::populateData(const QList<QString> &A,const QList<QString> &B,const QList<QString> &C)
+void TestModel::populateData(const QList<QString> &A,const QList<QString> &B,const QList<QString> &C,const QList<QString> &D,const QList<QString> &E)
 {
     tm_A.clear();
     tm_A = A;
@@ -34,6 +34,10 @@ void TestModel::populateData(const QList<QString> &A,const QList<QString> &B,con
     tm_B = B;
     tm_C.clear();
     tm_C = C;
+    tm_D.clear();
+    tm_D = D;
+    tm_E.clear();
+    tm_E = E;
     return;
 }
 
@@ -46,7 +50,7 @@ int TestModel::rowCount(const QModelIndex &parent) const
 int TestModel::columnCount(const QModelIndex &parent) const
 {
     Q_UNUSED(parent);
-    return 3;
+    return 5;
 }
 
 QVariant TestModel::data(const QModelIndex &index, int role) const
@@ -60,6 +64,10 @@ QVariant TestModel::data(const QModelIndex &index, int role) const
         return tm_B[index.row()];
     }else if (index.column() == 2) {
         return tm_C[index.row()];
+    }else if (index.column() == 3) {
+        return tm_D[index.row()];
+    }else if (index.column() == 4) {
+         return tm_E[index.row()];
     }
     return QVariant();
 }
@@ -72,8 +80,13 @@ QVariant TestModel::headerData(int section, Qt::Orientation orientation, int rol
         } else if (section == 1) {
             return QString("B");
         }else if (section == 2) {
+            return QString("C");
+        }else if (section == 3) {
+            return QString("D");
+        }else if (section == 4) {
             return QString("Output");
         }
+
     }
     return QVariant();
 }
@@ -92,62 +105,129 @@ void MainWindow::on_pushButton_clicked()
        QList<QString> A;
         QList<QString> B;
         QList<QString> C;
+        QList<QString> D;
+        QList<QString> E;
+
 
         // Create some data that is tabular in nature:
-     A.append("0");
-     B.append("0");
-     /* A.append("0");
-        A.append("0");
-        A.append("1");
-        A.append("1");
-        B.append("0");
-        B.append("1");
-       B.append("0");
-       B.append("1");*/
        using namespace puskar;
        expn();
-       for (int i=1;i<4;i++)
-      { for (int j=0;j<1;j++)
-       {
-           QString t = QString::number(X[i][j]);
-           QString u = QString::number(X[i][j+1]);
+       if (n==2)
+         {
+
+         for (int z=0;z<4;z++)
+         { for (int j=0;j<1;j++)
+         {
+           QString t = QString::number(X[z][j]);
+           QString u = QString::number(X[z][j+1]);
+          //  QString v = QString::number(X[z][j+2]);
+          //   QString w = QString::number(X[z][j+3]);
           A.append(t);
            B.append(u);
-       }
+            C.append("*");
+             D.append("*");
+          }
            }
-       for (int i=0;i<4;i++)
-       {        
+          for (int i=0;i<4;i++)
+          {
            QString s = QString::number(result[i]);
-          C.append(s);
+          E.append(s);
+           }
+
+
+           // Create model:
+           TestModel *truthtable = new TestModel(this);
+
+           // Populate model with data:
+           truthtable->populateData(A,B,C,D,E);
        }
 
 
-        // Create model:
-        TestModel *truthtable = new TestModel(this);
 
-        // Populate model with data:
-        truthtable->populateData(A,B,C);
+          else if (n==3)
+            {
+                for (int z=0;z<8;z++)
+             { for (int j=0;j<1;j++)
+              {
+               QString t = QString::number(X[z][j]);
+               QString u = QString::number(X[z][j+1]);
+                QString v = QString::number(X[z][j+2]);
+                 //QString w = QString::number(X[z][j+3]);
+              A.append(t);
+               B.append(u);
+                C.append(v);
+                 D.append("*");
+               }
+               }
+              for (int i=0;i<8;i++)
+              {
+               QString s = QString::number(result[i]);
+              E.append(s);
+              }
 
-        // Connect model to table view:
-        ui->tableView->setModel(truthtable);
 
-        // Make table header visible and display table:
-        ui->tableView->horizontalHeader()->setVisible(true);
-        ui->tableView->show();
+               // Create model:
+               TestModel *truthtable = new TestModel(this);
+
+                // Populate model with data:
+               truthtable->populateData(A,B,C,D,E);
+                }
+
+
+
+
+               else if (n==4)
+               {
+                for (int z=0;z<16;z++)
+               { for (int j=0;j<1;j++)
+             {
+              QString t = QString::number(X[z][j]);
+              QString u = QString::number(X[z][j+1]);
+              QString v = QString::number(X[z][j+2]);
+              QString w = QString::number(X[z][j+3]);
+          A.append(t);
+           B.append(u);
+            C.append(v);
+             D.append(w);
+             }
+           }
+            for (int i=0;i<16;i++)
+           {
+           QString s = QString::number(result[i]);
+          E.append(s);
+           }
+
+
+            // Create model:
+           TestModel *truthtable = new TestModel(this);
+
+            // Populate model with data:
+            truthtable->populateData(A,B,C,D,E);
+
+
+            // Connect model to table view:
+           ui->tableView->setModel(truthtable);
+          }
+
+           // Make table header visible and display table:
+      ui->tableView->horizontalHeader()->setVisible(true);
+      ui->tableView->show();
 }
 
 
 
-void Expression::get()
+void expression::get()
 {
     using namespace puskar;
     expression=S;
 }
-void Expression::analyze()
+void expression::analyze()
 {
 
     length = expression.length();
+    n=0;
 
+//finding total variables by excluding operators
 
     for (int i = 0; i < length; i++)
     {
@@ -159,7 +239,7 @@ void Expression::analyze()
 
     }
 
-    n2 = n;
+//finding total variables, where a variable is present only one time
 
     char variable[n];
 
@@ -189,7 +269,8 @@ void Expression::analyze()
    }
 
    n = a;
-
+cout<<"Hello "<<n;
+//copying variable to single_variable
 for(int i=0;i<a;i++)
 {
    cout<<variable[i]<<"\t";
