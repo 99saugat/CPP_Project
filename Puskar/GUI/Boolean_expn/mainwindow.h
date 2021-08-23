be25f5e19a8a6442ddf164d8f54888ca5c808209
Puskar-Neupane <84566897+Puskar-Neupane@users.noreply.github.com>
2021-08-23 11:53:01 +0545

    Create mainwindow.h

---
+++ Puskar/GUI/Boolean_expn/mainwindow.h
@@ -0,0 +1,64 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QAbstractTableModel>
+#include<QString>
+
+
+
+
+int expn();
+namespace Ui {
+    class MainWindow;
+}
+
+class TestModel : public QAbstractTableModel
+{
+    Q_OBJECT
+
+public:
+    TestModel(QObject *parent = 0);
+
+    void populateData(const QList<QString> &A,const QList<QString> &B,const QList<QString> &C,const QList<QString> &D,const QList<QString> &E);
+
+    int rowCount(const QModelIndex &parent = QModelIndex()) const Q_DECL_OVERRIDE;
+    int columnCount(const QModelIndex &parent = QModelIndex()) const Q_DECL_OVERRIDE;
+
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const Q_DECL_OVERRIDE;
+    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const Q_DECL_OVERRIDE;
+
+private:
+    QList<QString> tm_A;
+    QList<QString> tm_B;
+    QList<QString> tm_C;
+     QList<QString> tm_D;
+       QList<QString> tm_E;
+
+
+};
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_Expression_editingFinished();
+
+
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
