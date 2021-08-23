012a598be060646014eedb54840f53b48e3a51b7
Puskar-Neupane <84566897+Puskar-Neupane@users.noreply.github.com>
2021-08-23 11:52:41 +0545

    Create main.cpp

---
+++ Puskar/GUI/Boolean_expn/main.cpp
@@ -0,0 +1,27 @@
+/* Written by Puskar Neupane*/
+
+#include "mainwindow.h"
+#include "Expression.h"
+#include<iostream>
+#include<cmath>
+#include<string.h>
+using namespace std;
+
+#include <QApplication>
+int main(int argc, char *argv[])
+{
+
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+
+}
+
+
+
+
+
+
+
+
